4fc24486f67b8c97c89314a75e25e1d1dd18bfe8
Arjun_Krishna_Babu <arjunkbabu@users.noreply.github.com>
2015-02-16 21:40:07 +0530

    First Commit: Threaded Matrix Addition

---
+++ MatrAdd_Thread/MainClass.java
@@ -0,0 +1,115 @@
+/*
+	Java program to add two matrices using thread.
+
+	216B15 -- Arjun Krishna Babu
+*/
+
+import java.util.*;
+
+class Adder implements Runnable	{
+	/*
+		Function takes in a row, adds the row, and returns the sum
+	*/
+	
+	Thread T;
+	int sum;
+	int []M;
+	
+	int r, c;
+	
+	int i, j;
+	
+	Adder(int []M, int c)	{
+		this.M = M;
+		this.r = r;
+		this.c = c;
+		
+		sum = 0;
+		
+		T = new Thread(this);
+		T.start();				
+	}
+	
+	public void run()	{
+		for(j = 0; j < c; j++)	{
+			sum += M[j];
+		}
+	}
+	
+	public int getSum()	{
+		return sum;
+	}
+}
+
+public class MainClass	{
+	
+	public static void main(String []args)	{
+		Scanner in = new Scanner(System.in);
+		Random rand = new Random();
+		
+		Adder []add;
+		
+		int row, col;
+		int [][]A;
+		int []R;
+		int i, j;
+		long startTime, totTime;
+		
+		System.out.print("\n Enter row size: ");
+		row = rand.nextInt(20);
+
+		System.out.print("\n Enter col size: ");
+		col = rand.nextInt(20);
+
+		A = new int[row][col];
+
+		R = new int[row];
+		
+//		System.out.print("\n Enter the elements of matrix-A: ");
+		for(i = 0; i < row; i++)	{
+			for(j = 0; j < col; j++)	{
+				A[i][j] = rand.nextInt(20);
+			}
+		}
+ 
+		System.out.println("\n Matrix A[" + row + "] [" + col + "]: ");
+		for(i = 0; i < row; i++)	{
+			System.out.print("\n");
+			for(j = 0; j < col; j++)	{
+				System.out.print(" " + A[i][j]);
+			}
+		}
+
+		//compute sum
+
+		add = new Adder[row];
+		
+		startTime = System.nanoTime();
+		for(i = 0; i < row; i++)	{
+			for(j = 0; j < col; j++)	{
+				add[i] = new Adder(A[i], col);
+			}
+		}
+		
+		startTime = System.nanoTime();
+		for(i = 0; i < row; i++)	{
+			try {
+				add[i].T.join();				
+			}
+			catch (Exception E)		{	}
+		}
+		
+		totTime = System.nanoTime() - startTime;
+		
+		
+	
+		System.out.println("\n Sum of rows: ");
+		for(i = 0; i < row; i++)	{
+			System.out.print("\n");
+			System.out.print(" " + add[i].getSum() );
+		}
+		
+		System.out.println("\n Time taken: " + totTime);
+
+	}
+}
