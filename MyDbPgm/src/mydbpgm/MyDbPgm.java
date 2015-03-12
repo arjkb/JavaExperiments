5b1f72edcc65223f6a4723d1bc992afc9d37c11b
Arjun Krishna Babu <arjunkbabu@users.noreply.github.com>
2015-03-12 18:49:25 +0530

    Rudimentary functionality for searching a record in DB. Improvements needed

--- MyDbPgm/src/mydbpgm/MyDbPgm.java
+++ MyDbPgm/src/mydbpgm/MyDbPgm.java
@@ -7,9 +7,11 @@ package mydbpgm;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 /**
  *
@@ -20,14 +22,85 @@ public class MyDbPgm {
     /**
      * @param args the command line arguments
      */
+    
+    /*
+    static void insRecord() {
+        Connection con = null;
+        Statement s = null;
+        ResultSet rs = null;
+        
+        try {
+            con = DriverManager.getConnection("jdbc:derby://localhost:1527/MyDemoDB", "vesper", "bond");
+            s = con.createStatement();
+            //rs = s.executeQuery("select * from VESPER.STUDENT");
+            
+            while(rs.next())
+                System.out.println("\n Result: " + rs.getString(2));
+                 
+            
+        } catch(SQLException E) {
+            System.out.println("SQL Exception!");
+            E.printStackTrace();
+        }
+    }
+    */
+    
+    static void searchRecord(String id) {
+        Connection con = null;
+        Statement s = null;
+        ResultSet rs = null;
+        
+        PreparedStatement searchStmt = null;
+        
+//        String searchString = "select * from VESPER.STUDENT where ID like \'" + id +"\'";
+        
+        String searchString = "select * from VESPER.STUDENT where ID like ?";
+       
+        try {
+            con = DriverManager.getConnection("jdbc:derby://localhost:1527/MyDemoDB", "vesper", "bond");
+            searchStmt = con.prepareStatement(searchString);
+            searchStmt.setString(1, id);
+            //rs = s.executeQuery("select * from VESPER.STUDENT");
+            
+            rs = searchStmt.executeQuery();
+            if(rs.next())   {
+                System.out.println("\n Name: " + rs.getString(2));
+            }
+       
+            
+        } catch(SQLException E) {
+            System.out.println("SQL Exception!");
+            E.printStackTrace();
+        }
+    }
+    
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner num = new Scanner(System.in);
+        
+        int choice;
         System.out.println("\n Hello World!");
         
         Connection con = null;
         Statement s = null;
         ResultSet rs = null;
+        
+        
+        do {
+            System.out.println("\n 1: Insert student record");
+            System.out.println("\n 2: Search student record");
+            System.out.println("\n 0: QUIT");
+            System.out.println("\n Enter Your Choice: ");
+            choice = num.nextInt();
+            switch(choice)  {
+                case 2: searchRecord("101");
+                        break;
+                default: System.out.println("\n ERROR: Invalid Choice!");
+            }
             
+        } while (choice != 0);
+/*        
         try {
             con = DriverManager.getConnection("jdbc:derby://localhost:1527/MyDemoDB", "vesper", "bond");
             s = con.createStatement();
@@ -41,6 +114,7 @@ public class MyDbPgm {
             System.out.println("SQL Exception!");
             E.printStackTrace();
         }
+*/       
     }
     
 }
