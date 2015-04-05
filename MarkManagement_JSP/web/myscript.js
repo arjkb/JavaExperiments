790203ce5e0c5db8ea0bd2fe05e8a8c4651e07f6
Arjun Krishna Babu <arjunkbabu@users.noreply.github.com>
2015-04-05 08:06:17 +0530

    Script to modify action attribute of form

---
+++ MarkManagement_JSP/web/myscript.js
@@ -0,0 +1,18 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function setLink()  {
+    var logintype = document.loginform.usertype.value;
+    var formhandle = document.loginform;
+    
+    if( logintype = "admin")    {
+        formhandle.action = "adminsheet.jsp";
+    } else if( logintype = "stud" ) {
+        formhandle.action = "studsheet.jsp";        
+    }
+    
+    formhandle.submit;
+
+}
