1f1c1e1e66147a5d64772cec13db2b316bc5a2f9
Arjun Krishna Babu <arjunkbabu@users.noreply.github.com>
2015-04-06 19:23:35 +0530

    Script to validate password fields. Not yet functional

--- MarkManagement_JSP/web/myscript.js
+++ MarkManagement_JSP/web/myscript.js
@@ -3,16 +3,30 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+function checkPass()    {
+    //function to ensure that password and retype fields do infact
+    //contain the same passwords
+    
+    var pass1 = document.regform.pass1.value;
+    var pass2 = document.regform.pass2.value;
+    
+    if(pass1 === pass2)  {
+        console.log("Identical passwords");
+    } else {
+        console.log("Different passwords");
+    }
+}
+
 function setLink()  {
     var logintype = document.loginform.usertype.value;
     var formhandle = document.loginform;
     
-    if( logintype == "admin")    {
+    if( logintype === "admin")    {
         formhandle.action = "adminsheet.jsp";
-    } else if( logintype == "stud" ) {
+    } else if( logintype === "stud" ) {
         formhandle.action = "studsheet.jsp";        
     }
     
     formhandle.submit();
-
 }
