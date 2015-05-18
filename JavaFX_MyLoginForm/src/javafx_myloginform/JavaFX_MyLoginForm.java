edbfb9bfe8e518948603d5922576d6126fcb88a2
Arjun Krishna Babu <arjunkbabu@users.noreply.github.com>
2015-05-18 15:54:21 +0530

    Changed the start() for setting up the stage

--- JavaFX_MyLoginForm/src/javafx_myloginform/JavaFX_MyLoginForm.java
+++ JavaFX_MyLoginForm/src/javafx_myloginform/JavaFX_MyLoginForm.java
@@ -6,11 +6,6 @@
 package javafx_myloginform;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -20,25 +15,9 @@ import javafx.stage.Stage;
 public class JavaFX_MyLoginForm extends Application {
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public void start(Stage primaryStage)   {
+            primaryStage.setTitle("JavaFX Welcome!");
+            primaryStage.show();
     }
 
     /**
