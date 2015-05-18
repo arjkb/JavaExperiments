94daa0ce5e6cb5182e03cd1d5ea91102b09f8f1f
Arjun Krishna Babu <arjunkbabu@users.noreply.github.com>
2015-05-18 23:08:52 +0530

    Added forms elements

--- JavaFX_MyLoginForm/src/javafx_myloginform/JavaFX_MyLoginForm.java
+++ JavaFX_MyLoginForm/src/javafx_myloginform/JavaFX_MyLoginForm.java
@@ -5,7 +5,18 @@
  */
 package javafx_myloginform;
 
+
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -17,6 +28,40 @@ public class JavaFX_MyLoginForm extends Application {
     @Override
     public void start(Stage primaryStage)   {
             primaryStage.setTitle("JavaFX Welcome!");
+            
+            //setup the GridPane layout
+            GridPane grid = new GridPane();
+            grid.setAlignment(Pos.CENTER);
+            
+            //manage the spacing between rows and columns
+            grid.setHgap(10);
+            grid.setVgap(10);
+
+            //space around the edges of the grid pane
+            grid.setPadding(new Insets(25, 25, 25, 25));
+            
+            //create the scene with grid as root node
+            Scene scene = new Scene(grid, 300, 275);
+            
+            
+            //set the title for the page, along with font
+            Text sceneTitle = new Text("Welcome");
+            sceneTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+            grid.add(sceneTitle, 0, 0, 2, 1);
+            
+            //set the username label at zeroth col, first row
+            Label userName = new Label("Username");
+            grid.add(userName, 0, 1);
+            
+            TextField userTextField = new TextField();
+            grid.add(userTextField, 1, 1);           
+            
+            Label pw = new Label("Password");
+            grid.add(pw, 0, 2);
+            
+            PasswordField pwBox = new PasswordField();
+            grid.add(pwBox, 1, 2);
+                        
             primaryStage.show();
     }
 
